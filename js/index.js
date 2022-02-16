8269ddff99b796a2ef2b916fe3d1652eed9a3eb4
noormohammad20 <mdsaim204@gmail.com>
2022-02-17 00:09:59 +0600

    try to code functional

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log('hello')
+function getInputValue(input) {
+    const inputId = document.getElementById(input)
+    const inputValue = parseInt(inputId.value)
+    return inputValue;
+
+}
+
+function getInnerText(innerText) {
+    const innerTextId = document.getElementById(innerText)
+    const innerTextValue = Number(innerTextId.innerText)
+    return innerTextValue
+}
+
+
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const incomeInput = getInputValue('income-input')
+
+    const foodInput = getInputValue('food-input')
+    const rentInput = getInputValue('rent-input')
+    const clothesInput = getInputValue('clothes-input')
+
+    const expenses = foodInput + rentInput + clothesInput;
+
+    const balance = incomeInput - expenses
+
+
+
+})
