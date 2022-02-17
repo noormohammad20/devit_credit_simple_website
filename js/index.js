f0977f4322864093c5ffb50030680bf15884ddce
noormohammad20 <mdsaim204@gmail.com>
2022-02-17 12:29:53 +0600

    save bottun code done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 function getInputValue(input) {
     const inputId = document.getElementById(input)
-    const inputValue = parseInt(inputId.value)
-    return inputValue;
+    if (inputId.value < 0) {
+        alert('please enter positive value')
+
+    }
+    else {
+        const inputValue = parseInt(inputId.value)
+        return inputValue;
+    }
 
 }
 
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
+    // get input value 
     const incomeInput = getInputValue('income-input')
-
 
     const foodInput = getInputValue('food-input')
     const rentInput = getInputValue('rent-input')
@@ -23,3 +29,23 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     document.getElementById('balance-amount').innerText = totalBalance;
 })
 
+document.getElementById('save-btn').addEventListener('click', function () {
+
+    const incomeInput = getInputValue('income-input')
+    const savingInput = getInputValue('saving-input')
+
+
+    const savingTotal = (incomeInput * savingInput) / 100
+
+    const savingAmount = document.getElementById('saving-amount').innerText = savingTotal;
+
+    const balanceAmount = document.getElementById('balance-amount').innerText;
+
+
+
+    const balance = balanceAmount - savingAmount;
+    document.getElementById('remaining-balance').innerText = balance;
+
+
+
+})
